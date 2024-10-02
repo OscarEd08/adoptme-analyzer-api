eb8d937150273cffc9b87f5fdab9fdf9fd993d2b
NavyyBlue <jean.cardenas2@unmsm.edu.pe>
2024-10-02 16:18:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from fastapi import FastAPI
-
-# Inicializar la aplicación FastAPI
-app = FastAPI()
-
-# Incluir routers de las APIs
 # from src.aws.router import router as aws_router
 from src.openai.router import router as openai_router
+
+app = FastAPI()
 
 # app.include_router(aws_router, prefix="/aws", tags=["aws"])
 app.include_router(openai_router, prefix="/openai", tags=["openai"])
