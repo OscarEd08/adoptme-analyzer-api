1a6ed2d385dfeb26abceeae733a65580319130a4
NavyyBlue <jean.cardenas2@unmsm.edu.pe>
2024-10-25 18:15:52 -0500

    Update constants.py

--- src/openai/constants.py
+++ src/openai/constants.py
@@ -1,8 +1,8 @@
 PROMPT_PET_FEATURES = (
     "Por favor, mira la imagen de la mascota que se encuentra adjunta y proporciona las siguientes "
-    "características separadas por punto coma en una sola linea: species; breed; age(Dame un numero); "
+    "características separadas por punto coma en una sola linea: species; breed; age(Dame un numero en meses); "
     "weight (en kilogramos, solo el numero); color; size (pequeño, mediano, grande). Como por ejemplo: "
-    "species: Perro; breed: Mestizo; age: 5; weight: 25; color: Marrón claro y negro; size: Mediano. "
+    "species: Perro; breed: Mestizo; age: 60; weight: 25; color: Marrón claro y negro; size: Mediano. "
     "Si no existe ninguna mascota en la imagen devolver el siguiente mensaje: No existe ninguna mascota."
 )
 
